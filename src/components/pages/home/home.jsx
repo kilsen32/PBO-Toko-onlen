525593bab213047f5428ec125fa73ccf2049732d
kilsen32 <aditpasker9@gmail.com>
2023-10-05 14:40:28 +0700

    useState

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,20 +1,37 @@
-import Navbar from "../../layouts/navbar/navbar.jsx"
-import Iklan from "../../layouts/iklan/iklan.jsx"
-import Cards from "../../layouts/cards/cards.jsx"
-import Promosi from "../../layouts/promosi/promosi.jsx"
-import Footer from "../../layouts/footer/footer.jsx"
+import Navbar from "../../layouts/navbar/navbar.jsx";
+import Iklan from "../../layouts/iklan/iklan.jsx";
+import Cards from "../../layouts/cards/cards.jsx";
+import Promosi from "../../layouts/promosi/promosi.jsx";
+import Footer from "../../layouts/footer/footer.jsx";
+import { useState, useEffect } from "react";
 
+function home() {
+  const { laptop, setLaptop } = useState([]);
 
-function home(){
-    return(
-        <div>
-            <Navbar/>
-            <Iklan/>
-            <Cards/>
-            <Promosi/>
-            <Footer/>
-        </div>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../data/product.json");
+        const jsonData = await response.json();
+        const laptops = jsonData.laptop;
+        setLaptop(laptops);
+      } catch (error) {
+        console.error("error ffetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(laptop);
+
+  return (
+    <div>
+      <Navbar />
+      <Iklan />
+      <Cards />
+      <Promosi />
+      <Footer />
+    </div>
+  );
 }
 
 export default home;
